165ab7c7f1c8e83c27e4f27783eee292ca983a86
Nat-Thesratanavong <nat.thesratanavong@gmail.com>
2025-11-19 23:10:04 +0700

    added method and modify method to get content from epub.

--- Ebook_reader/parsing/epub_parser.py
+++ Ebook_reader/parsing/epub_parser.py
@@ -1,3 +1,4 @@
+import ebooklib 
 from ebooklib import epub
 from .ebook_parser import ebook_parser
 
@@ -7,7 +8,8 @@ class epub_parser(ebook_parser):
         super().__init__(file_path)
 
         self.book = None
-        self.meta = None
+        self._meta = None
+        self._chapters = []
 
     def read_ebook(self):
         if not self.validate_path():
@@ -16,26 +18,54 @@ class epub_parser(ebook_parser):
         try:
             book = epub.read_epub(self.file_path)
 
-            self.book = book
+            self._book = book
+            self._load_meta()
+            self._load_chapters()
             
 
             return True
         except Exception as e:
             print(f"Error: could not read EPUB file at {self.file_path}. Details: {e}")
-            self.book = None
+            self._book = None
         return False
     
-    def get_meta(self):
-        if not self.book:
+    
+    
+    def _load_meta(self):
+        if not self._book:
             return None
         
         try:
-            meta = self.book.metadata
-            self.meta = meta
-            if not self.meta:
+            meta = self._book.metadata
+            self._meta = meta
+            if not self._meta:
                 return False
             return True
         except Exception as e:
             print(f"Error: oculd not read EPUB meta data at {self.file_path}. Details: {e}")
-            self.meta = None
+            self._meta = None
         return False
+    
+    def _load_chapters(self):
+        for item in self._book.get_items():
+            if item.get_type() == ebooklib.ITEM_DOCUMENT:
+                self._chapters.append(item)
+        return True
+    def get_meta(self):
+            if(self._meta):
+                meta = self._meta
+            else:
+                meta = None
+            return meta
+    
+    def get_chapter(self, target_chapter):
+        if(target_chapter < 0 | target_chapter > len(self._chapters)):
+            return None
+        try:
+            chapter = self._chapters[target_chapter]
+            raw_data = chapter.get_content()
+            html_string = raw_data.decode('utf-8')   
+            return html_string
+        except Exception as e:
+            print(f"Error: oculd not read EPUB meta data at {self.file_path}. Details: {e}")
+            return None
--- Ebook_reader/tests/test_parsing.py
+++ Ebook_reader/tests/test_parsing.py
@@ -5,7 +5,11 @@ def test_epub_parser_can_read_file():
     test_file_path = "./Ebook_reader/tests/sample_files/pg100-images.epub"
     parser = create_parser(test_file_path)
     success = parser.read_ebook()
-    parser.get_meta()
+    # print(parser.get_chapter(2))
+    
+    
 
     assert success == True
-    assert parser.meta
+    assert parser._meta
+    assert parser._chapters
+    
